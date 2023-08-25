a907bd624023c9796c11fb953f2da84d4fddef1c
Javier Guzman <906599+javierguzman@users.noreply.github.com>
2023-08-25 07:03:55 +0200

    Sentry stuff

---
+++ next.config.js
@@ -0,0 +1,48 @@
+/** @type {import('next').NextConfig} */
+const { withSentryConfig } = require("@sentry/nextjs");
+
+const nextConfig = {
+  reactStrictMode: true,
+  ...(process.env.NODE_ENV === "production" ? { output: "standalone" } : {}),
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+  sentry: {
+    // For all available options, see:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+    // Upload a larger set of source maps for prettier stack traces (increases build time)
+    widenClientFileUpload: true,
+
+    // Transpiles SDK to be compatible with IE11 (increases bundle size)
+    transpileClientSDK: true,
+
+    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
+    tunnelRoute: "/monitoring",
+
+    // Hides source maps from generated client bundles
+    hideSourceMaps: true,
+
+    // Automatically tree-shake Sentry logger statements to reduce bundle size
+    disableLogger: true,
+  },
+};
+
+// Injected content via Sentry wizard below
+
+const sentryWebpackPluginOptions = {
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options
+
+  // Suppresses source map uploading logs during build
+  silent: true,
+
+  org: "whatever",
+  project: "whatever",
+};
+
+module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions);
